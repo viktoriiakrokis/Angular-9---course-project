90dca8fd165900ce49850502c9d459d1a660b678
Viktoriia Krokis <viktoriia@iwa.fi>
2020-06-21 11:43:23 +0300

    create own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { LogInFormComponent } from './log-in-form/log-in-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    LogInFormComponent
   ],
   imports: [
     BrowserModule
