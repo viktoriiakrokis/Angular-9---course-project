7458993b22fc988414d311e484ab2c349e6db206
Viktoriia Krokis <vika.krokis063@gmail.com>
2020-06-21 20:44:30 +0300

    ngStyle - change style dynamicly

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
     serverId: number = 10;
-    serverStatus: string = "offline";
+    serverStatus: string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+    }
 
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
