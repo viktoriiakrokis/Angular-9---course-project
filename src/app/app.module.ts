1bd3150f1a5fb81b128d34029963dba9258f8c8d
Viktoriia Krokis <vika.krokis063@gmail.com>
2020-06-21 18:23:23 +0300

    two way databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -14,7 +15,8 @@ import { LogInFormComponent } from './log-in-form/log-in-form.component';
     LogInFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
