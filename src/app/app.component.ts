a7de609e3b21d7cd9940819168e082c08b1cd7bd
Viktoriia Krokis <vika.krokis063@gmail.com>
2020-06-30 22:32:03 +0300

    form group and form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 import { Validator } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,15 @@ import { Validator } from '@angular/forms';
 export class AppComponent {
 
   constructor(private fb: FormBuilder) {}
+
+  email = new FormControl('');
+
+  userForm = new FormGroup({
+    name1: new FormControl('', Validators.required),
+    name2: new FormControl(''),
+    age: new FormControl(''),
+    email: new FormControl('')
+  })
 
   userprofileForm = this.fb.group({
     firstName: ['', Validators.required],
@@ -32,5 +42,15 @@ export class AppComponent {
     console.log("last name is" + lastName);
 
     console.log("address one is" + this.userprofileForm.get(['address', 'address1']).value);
+  }
+
+  updateEmail() {
+    this.email.setValue('test@testdomain.com');
+  }
+
+  onSubmit2() {
+    console.log(this.userForm.value);
+    console.log(this.userForm.controls['firstName'].value);
+    console.log(this.userForm.get('fistName').value);
   }
 }
